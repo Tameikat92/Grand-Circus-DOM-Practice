9248a621e3207bbbf543d1580763a769ce36e874
Tameika Thompson <meekmeek92@MacBook-Pro-82.local>
2024-09-16 20:23:09 -0400

    dom 3 function

---
+++ DOM example 3/script.js
@@ -0,0 +1,21 @@
+
+
+const userForm = document.getElementById('userForm');
+const userNameInput = document.getElementById('username');
+const passWordInput = document.getElementById('password');
+const formOutput = document.getElementById('formOutput');
+
+console.log("Script Loaded: " + new Date().toDateString());
+
+userForm.addEventListener('submit', function(event){
+
+event.preventDefault(); // dont do default behavior when page refreshes
+
+const enterUsername = userNameInput.value;
+const enteredPassword = passWordInput.value;
+
+formOutput.textContent = "Username:" + enterUsername + 'Password' + enteredPassword;
+})
+
+
+
